20264e9dacc25c9ca6d0db0e2e93c74407065d51
AdiSingh003 <aprataps710@gmail.com>
2024-02-21 01:58:28 +0530

    Promisify tasks

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -3,8 +3,22 @@
  * During this time the thread should not be able to do anything else.
  * the function should return a promise just like before
  */
-
+const start = Date.now();
 function sleep(milliseconds) {
+    return new Promise(function(resolve){
+        setTimeout(() => {
+        }, milliseconds);
+        setTimeout(() => {
+        resolve()
+        }, milliseconds);    
+    })      
+
+}
+async function main(){
+        await sleep() 
+        const end = Date.now();
+        const difference = end - start;
+        console.log(difference)
 }
 
 module.exports = sleep;
