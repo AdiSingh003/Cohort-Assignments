6c48f74dc45e9b09c90147eaa9a339181d8fa6fc
AdiSingh003 <aprataps710@gmail.com>
2024-02-28 00:57:11 +0530

    week3 middleware tasks

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -12,10 +12,25 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
-setInterval(() => {
+setTimeout(() => {
     numberOfRequestsForUser = {};
 }, 1000)
-
+app.use(function(req,res,next){
+  const userId = req.headers['user-id']
+if(numberOfRequestsForUser[userId]){
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1
+  if(numberOfRequestsForUser[userId] >5){
+    res.status(404).send("No entry")
+  }
+  else{
+    next()
+  }
+}
+else{
+  numberOfRequestsForUser[userId] = 1
+  next()
+}
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
